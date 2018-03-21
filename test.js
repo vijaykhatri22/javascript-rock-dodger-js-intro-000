70a0f5e372f66269834d2d581094cdfe21eac5a1
vijay khatri <vijaykhatri22@gmail.com>
2018-03-21 14:23:02 +0000

    automatically backed up by learn

--- test.js
+++ test.js
@@ -33,7 +33,7 @@ function createRock(x) {
   rock.className = 'rock'
   rock.style.left = `${x}px`
   var top = 0
-  rock.style.top = `${top}px`;
+  rock.style.top = `${top+=2}px`;
 
   GAME.appendChild(rock);
 
