a368da8a21ccb197ee2e8e34f7f9d7837eaa322d
vijay khatri <vijaykhatri22@gmail.com>
2018-03-21 14:22:02 +0000

    automatically backed up by learn

--- test.js
+++ test.js
@@ -84,3 +84,16 @@ function moveDodgerRight(){
   }
   window.requestAnimationFrame(step);
 }
+function positionToInteger(p) {
+  return parseInt(p.split('px')[0]) || 0
+}
+
+function start() {
+  window.addEventListener('keydown', moveDodger)
+
+  START.style.display = 'none'
+
+  gameInterval = setInterval(function() {
+    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
+  }, 1000)
+}
