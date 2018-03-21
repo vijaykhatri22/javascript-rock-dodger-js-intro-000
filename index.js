9b49ccc225daf3ccf2e4668cfeca34e22731aa45
vijay khatri <vijaykhatri22@gmail.com>
2018-03-21 15:03:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,9 +64,10 @@ function endGame(){
 }
 function moveDodger(e){
   if(e.which == LEFT_ARROW){
-
-    moveDodgerLeft();
     e.preventDefault();
+    e.stopPropagation();
+    moveDodgerLeft();
+    
   }
   else if (e.which == RIGHT_ARROW) {
     moveDodgerRight();
