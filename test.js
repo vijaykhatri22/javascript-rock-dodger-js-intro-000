c43fc3e39a78c3289f73dacc753a6e53c3e5c77f
vijay khatri <vijaykhatri22@gmail.com>
2018-03-21 13:47:18 +0000

    automatically backed up by learn

--- test.js
+++ test.js
@@ -12,5 +12,18 @@ var gameInterval = null
 function checkCollision(rock) {
   const top = positionToInteger(rock.style.top)
   if (top > 360) {
+    //dodger
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
     const dodgerRightEdge = dodgerLeftEdge + 40;
+    //rock
+    const rockLeftEdge = positionToInteger(rock.style.left)
+    const rockRightEdge = rockLeftEdge + 20 ;
+
+    // actual collision check
+    if((rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge)||
+       (rockLeftEdge > dodgerLeftEdge && rockRightEdge < dodgerRightEdge)||
+       (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge)){
+         return true;
+       }  
+  }
+}
